6018c0fdd5d8f6e23167b6e5dfb1f1ba6838e549
Ali <alisamatov106@gmail.com>
2023-10-12 02:51:10 +0600

    created modal and backdrop

---
+++ src/components/UI/modal/UiModal.jsx
@@ -0,0 +1,35 @@
+import { Box, Modal, styled } from '@mui/material'
+
+export const UiModal = ({ open, onClose, children, ...rest }) => {
+   return (
+      <Backdrop open={open} onClose={onClose}>
+         <ModalStyle {...rest}>{children}</ModalStyle>
+      </Backdrop>
+   )
+}
+const ModalStyle = styled(Box)(({ ...rest }) => ({
+   position: 'fixed',
+   left: '50%',
+   borderRadius: '10px',
+   width: rest.width,
+   transform: 'translate(-50%, -50%)',
+   height: rest.height,
+   border: 'none',
+   backgroundColor: 'white',
+   padding: rest.padding || '16px 20px',
+   top: '50%',
+   maxHeight: '100vh',
+}))
+const Backdrop = styled(Modal)(({ ...rest }) => ({
+   position: 'fixed',
+   top: '0',
+   left: '0',
+   right: '0',
+   bottom: '0',
+   width: '100%',
+   height: '100vh',
+   backgroundColor: 'rgba(240, 230, 230, 0.288)',
+   backdropFilter: rest.backdropFilter || 'blur(2px)',
+   zIndex: '990',
+   // minHeight: '11.615rem',
+}))
