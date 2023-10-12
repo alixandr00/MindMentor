a3957c68b33c926d60f7155dd291f7372465c2cd
Zhakshylyk <zhakshylyknasipbekov@gmail.com>
2023-10-12 18:55:35 +0600

    created group cards

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,15 @@
 import { ReactComponent as GmailIcon } from './gmailIcon.svg'
 import { ReactComponent as PhoneIcon } from './phoneIcon.svg'
 import { ReactComponent as LocationIcon } from './locationIcon.svg'
+import { ReactComponent as ProgressIcon } from './progressIcon.svg'
+import { ReactComponent as NotesIcon } from './notesIcon.svg'
+import { ReactComponent as PeopleIcon } from './peopleIcon.svg'
 
-export { GmailIcon, PhoneIcon, LocationIcon }
+export {
+   GmailIcon,
+   PhoneIcon,
+   LocationIcon,
+   ProgressIcon,
+   NotesIcon,
+   PeopleIcon,
+}
