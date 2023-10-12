d44b9518deb7cf755c1dd1d0cc56d5596f5ae171
Ali <alisamatov106@gmail.com>
2023-10-12 20:41:38 +0600

    created interns

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,8 @@
 import { ReactComponent as GmailIcon } from './gmailIcon.svg'
 import { ReactComponent as PhoneIcon } from './phoneIcon.svg'
 import { ReactComponent as LocationIcon } from './locationIcon.svg'
+import { ReactComponent as SearchIcon } from './search-normal.svg'
+import { ReactComponent as DownIcon } from './arrow-down.svg'
+import { ReactComponent as UpIcon } from './up.svg'
 
-export { GmailIcon, PhoneIcon, LocationIcon }
+export { GmailIcon, PhoneIcon, LocationIcon, SearchIcon, DownIcon, UpIcon }
