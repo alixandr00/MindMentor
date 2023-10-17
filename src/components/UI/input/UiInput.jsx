7bebfb723f15d47cd802be67108763fd183c1bf0
Tugolbay Kasymbekov <kasymbekovv1221@gmail.com>
2023-10-17 16:00:30 +0600

    save but not finished VendorsModal

--- src/components/UI/input/UiInput.jsx
+++ src/components/UI/input/UiInput.jsx
@@ -1,55 +1,100 @@
-import { styled } from '@mui/material/styles'
-import TextField from '@mui/material/TextField'
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
+import { IconButton, OutlinedInput, styled } from '@mui/material'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 
 export const UiInput = forwardRef(
    (
-      { label, type, id, value, onChange, border, placeholder, ...other },
+      {
+         error,
+         color,
+         type,
+         id,
+         placeholder,
+         value,
+         border,
+         onChange,
+         onBlur,
+         padding,
+         ...props
+      },
       ref
    ) => {
+      const [showPassword, setShowPassword] = useState(false)
+
+      const handleClickShowPassword = () => setShowPassword((show) => !show)
+
+      const passwordType = showPassword ? 'text' : 'password'
+
+      const handleBlur = (event) => {
+         if (onBlur) {
+            onBlur(event)
+         }
+      }
+
       return (
-         <MyStyledInput
-            size="small"
-            label={label}
-            type={type}
-            id={id}
-            value={value}
-            onChange={onChange}
-            border={border}
-            placeholder={placeholder}
-            ref={ref}
-            {...other}
-         />
+         <div>
+            <InputOutlained
+               {...props}
+               value={value}
+               onChange={onChange}
+               error={error}
+               color={color}
+               placeholder={placeholder}
+               id={id}
+               ref={ref}
+               border={border}
+               padding={padding}
+               onBlur={handleBlur}
+               type={type === 'password' ? passwordType : type}
+               {...props}
+               endAdornment={
+                  type === 'password' ? (
+                     <StyleIconButton onClick={handleClickShowPassword}>
+                        {showPassword ? (
+                           <VisibilityIcon />
+                        ) : (
+                           <VisibilityOffIcon />
+                        )}
+                     </StyleIconButton>
+                  ) : (
+                     ''
+                  )
+               }
+            />
+         </div>
       )
    }
 )
+UiInput.displayName = 'Input'
 
-const MyStyledInput = styled(TextField)(({ ...props }) => ({
-   input: {
-      padding: props.padding || 0,
-      width: props.width || '11.25rem',
-      backgroundColor: props.backgroundColor || 'white',
-      border: props.border || '1px solid transparent',
-      paddingLeft: props.paddingLeft || '15px',
-   },
-   '& .MuiInputBase-input': {
-      height: props.height || '2.25rem',
-   },
-   '& label': {
-      fontSize: '1rem',
-      fontFamily: 'CarePro',
-   },
-   '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-         border: props.border || '1px solid #B2B2B2',
-         borderRadius: props.borderRadius || '5px',
+const InputOutlained = styled(OutlinedInput)(
+   ({
+      width,
+      height,
+      padding,
+      fontSize,
+      background,
+      borderradius,
+      border,
+      borderColor,
+      ...props
+   }) => ({
+      width,
+      height,
+      padding,
+      fontSize,
+      background,
+      borderRadius: borderradius,
+      border: `1px solid ${borderColor}`,
+      '.MuiInputBase-input': {
+         padding: props.classpadding === 'true' && '3px 14px',
       },
-      '&:hover fieldset': {
-         borderColor: props.hoverBorderColor || '1px solid #000000',
-      },
-      '&.Mui-focused fieldset': {
-         borderColor: props.focusBorderColor || '1px solid green',
-         border: props.border || '1px solid #000000',
-      },
-   },
-}))
+   })
+)
+
+const StyleIconButton = styled(IconButton)`
+   /* position: absolute;
+   left: 26.25rem; */
+   display: flex;
+`
--- src/components/vendorsModal/VendorsModal.jsx
+++ src/components/vendorsModal/VendorsModal.jsx
@@ -1,12 +1,85 @@
-import { styled } from '@mui/material'
+import { Button, styled } from '@mui/material'
 import React from 'react'
+import { UiInput } from '../UI/input/UiInput'
+import { UiModal } from '../UI/modal/UiModal'
 
 export const VendorsModal = () => {
-   return <Container>h</Container>
+   return (
+      <ModalComponent open width="60.4375rem" height="47.1875rem">
+         <BlockInput>
+            <div>
+               <InputTitle>Company Name</InputTitle>
+               <UiInput
+                  width="30.8125rem"
+                  height="2.0625rem"
+                  type="text"
+                  borderColor="#fff"
+                  borderradius="0.626rem"
+               />
+            </div>
+            <div>
+               <InputTitle>Email</InputTitle>
+               <UiInput
+                  width="30.8125rem"
+                  height="2.0625rem"
+                  type="email"
+                  borderColor="#fff"
+                  borderradius="0.626rem"
+               />
+            </div>
+            <div>
+               <InputTitle>Address</InputTitle>
+               <UiInput
+                  width="30.8125rem"
+                  height="2.0625rem"
+                  type="text"
+                  borderColor="#fff"
+                  borderradius="0.626rem"
+               />
+            </div>
+            <div>
+               <InputTitle>Phone Number</InputTitle>
+               <UiInput
+                  width="30.8125rem"
+                  height="2.0625rem"
+                  type="tel"
+                  borderColor="#fff"
+                  borderradius="0.626rem"
+               />
+            </div>
+            <div>
+               <InputTitle>Phone Number</InputTitle>
+               <UiInput
+                  width="30.8125rem"
+                  height="6.625rem"
+                  type="text"
+                  borderColor="#fff"
+                  borderradius="0.626rem"
+               />
+            </div>
+            <div>
+               <Button>Delete</Button>
+               <Button>Save</Button>
+            </div>
+         </BlockInput>
+      </ModalComponent>
+   )
 }
 
-const Container = styled('div')`
-   width: 60.4375rem;
-   height: 47.1875rem;
+const BlockInput = styled('div')`
+   display: flex;
+   flex-direction: column;
+   gap: 1.12rem;
+   position: relative;
+`
+const InputTitle = styled('p')`
+   color: #fff;
+   font-size: 1.25rem;
+   font-weight: 500;
+   padding-bottom: 0.5rem;
+`
+const ModalComponent = styled(UiModal)`
+   display: flex;
+   justify-content: center;
    background-color: #1e1f22;
 `
