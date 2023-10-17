8c59ba2ad9ad30a28027b26faeb27c178dd09233
Tugolbay Kasymbekov <kasymbekovv1221@gmail.com>
2023-10-17 21:45:20 +0600

    save and finished VendorsModal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { NewVendorModal } from './components/vendorsModal/NewVendorModal'
 import { AdminLayout } from './layout/AdminLayout'
 
 function App() {
    return (
       <div>
-         <NewVendorModal />
          <AdminLayout />
       </div>
    )
